24e51b48db60335378bc31add9d0cd781bc464c1
Jack Dipper <jack@jackdipper.com>
2013-02-23 06:17:46 +0000

    Includes extra files needed for theme

---
+++ sites/default/themes/stf/node-news.tpl.php
@@ -0,0 +1,26 @@
+<?php if($teaser): ?>
+<div class="front node<?php if ($sticky) { print " sticky"; } ?><?php if (!$status) { print " node-unpublished"; } ?>">
+  <?php if ($page == 0) : ?>
+    <h2 class="title"><a href="<?php print $node_url?>"><?php print $title?></a></h2>
+  <?php endif; ?>
+  <div class="content"><?php print $content?>
+  <?php if ($links) : ?>
+    <span class="links">&raquo; <?php print $links?></span>
+  <?php endif; ?>
+  <span class="submitted"><?php print $submitted?></span>
+  <span class="taxonomy"><?php print $terms?></span>
+  </div>
+</div>
+<?php else: ?>
+<div class="node<?php if ($sticky) { print " sticky"; } ?><?php if (!$status) { print " node-unpublished"; } ?>">
+  <?php if ($page == 0) : ?>
+    <h2 class="title"><a href="<?php print $node_url?>"><?php print $title?></a></h2>
+  <?php endif; ?>
+  <span class="submitted"><?php print $submitted?></span>
+  <span class="taxonomy"><?php print $terms?></span>
+  <div class="content"><?php print $content?></div>
+  <?php if ($links) : ?>
+    <div class="links">&raquo; <?php print $links?></div>
+  <?php endif; ?>
+</div>
+<?php endif; ?>
